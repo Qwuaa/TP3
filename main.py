aa4a7a04364ebe8f0c15ab91653f9cf0fc8b5133
Qwuaa <arthur.beaupere@iia-formation.fr>
2025-04-03 12:28:15 +0200

    python dropper

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ app.add_middleware (
     CORSMiddleware, 
     allow_origins=orginis,
     allow_credentials=True, 
-    allow_methods=[*],
-    allow_headers=[*],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
